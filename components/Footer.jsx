b626fb381c2884b237545c84ac148611d9ceffc7
Vinicius Nascimento <viniciusnicacio80@gmail.com>
2023-12-05 13:51:07 -0300

    link de contato

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ import NavLogo from "../public/white-logo.svg";
 import Link from "next/link";
 function Footer() {
   return (
-    <>
+    <div>
       <div className="bg-green-700 h-1/2 w-full sm:mt-52 flex md:flex-row flex-col justify-around items-start p-20">
         <div className="p-5 ">
           <ul>
@@ -21,36 +21,36 @@ function Footer() {
                 width="170"
                 height="70"
                 className="cursor-pointer"
-              />
+              
             </a>
             <div className="flex justify-center gap-6 pb-5">
-              <Link href="https://www.instagram.com/">
-                <FaInstagram className="text-2xl text-white cursor-pointer hover:text-gray-300" />
-              </Link>
-              <FaFacebook className="text-2xl text-white cursor-pointer hover:text-gray-300" />
-              <FaWhatsapp className="text-2xl text-white cursor-pointer hover:text-gray-300" />
-              <Link href="https://www.linkedin.com/company/canaapallets">
-                <FaLinkedin className="text-2xl text-white cursor-pointer hover:text-gray-300" />
-              </Link>
-              <Link href="https://www.wikipedia.org">
+              <Link href="https://www.instagram.com/canaapallets/tagged/">
                 <a target="_blank" rel="noopener noreferrer">
-                  Open Wikipedia in a new tab
+                  <FaInstagram className="text-2xl text-white cursor-pointer hover:text-gray-300" />
+                </a>
+              </Link>
+
+              <Link href="https://www.facebook.com/p/Cana%C3%A3-Pallets-100054272239989/?locale=pt_BR">
+                <a target="_blank" rel="noopener noreferrer">
+                  <FaFacebook className="text-2xl text-white cursor-pointer hover:text-gray-300" />
+                </a>
+              </Link>
+
+              <Link href="https://www.youtube.com/">
+                <a target="_blank" rel="noopener noreferrer">
+                  <FaWhatsapp className="text-2xl text-white cursor-pointer hover:text-gray-300" />
+                </a>
+              </Link>
+
+              <Link href="https://www.linkedin.com/company/canaapallets">
+                <a target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin className="text-2xl text-white cursor-pointer hover:text-gray-300" />
                 </a>
               </Link>
             </div>
           </ul>
         </div>
-        <div className="p-5">
-          <ul>
-            <p className="text-white font-bold text-2xl pb-4">Empresa</p>
-            <li className="text-gray-300 text-md pb-2 font-semibold hover:text-red-600 cursor-pointer">
-              História
-            </li>
-            <li className="text-gray-300 text-md pb-2 font-semibold hover:text-red-600 cursor-pointer">
-              Missão
-            </li>
-          </ul>
-        </div>
+
         <div className="p-5">
           <ul>
             <p className="text-white font-bold text-2xl pb-4">Sobre nós</p>
@@ -69,15 +69,22 @@ function Footer() {
           </ul>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center text-center  p-5 bg-gray-50">
-        <h3 className=" text-gray-800 font-semibold">
-          © 2023 Todos os direitos reservados | Desenvolvido ❤ pela{" "}
-          <span className="hover:text-red-600 font-semibold cursor-pointer">
+      <div className="flex flex-col justify-center items-center text-center  p-5 bg-gray-50">                                      
+        <link href="https://www.linkedin.com/company/canaapallets">
+          <a target="_blank" rel="noopener noreferrer">
+            <h3 className=" text-gray-800 font-semibold" / >© 2023 Todos os
+            direitos reservados | Desenvolvido ❤ pela{" "}
+            <span className="hover:text-red-600 font-semibold cursor-pointer" />
             Sousas Midia{" "}
-          </span>
-        </h3>
+          </a>
+        </link>
+        
       </div>
-    </>
+      </div>
+    
+      
+  
+    
   );
-}
+};
 export default Footer;
