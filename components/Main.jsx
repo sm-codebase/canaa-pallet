4302064373acd136c2069819e996f6335da9b1d7
Vinicius Nascimento <viniciusnicacio80@gmail.com>
2023-12-06 17:57:54 -0300

    implantaçao novo carousel

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,50 +1,79 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import React from "react";
-import SlImgSm1 from "../public/assets/sl-img-sm-1.png";
-import SlImg1 from "../public/assets/sl-img-1.png";
-import MainSlider from "./MainSlider";
-import Certifications from "./Certifications";
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+import slimg1 from "../public/assets/carousel/pr-img-1.png";
+import slimg2 from "../public/assets/carousel/pr-img-2.png";
+import slimg3 from "../public/assets/carousel/pr-img-3.png";
+import slimg4 from "../public/assets/carousel/pr-img-4.png";
 
-const Main = () => {
+
+function App() {
+  const slides = [
+    {
+      image: slimg1,
+    },
+    {
+      image: slimg2,
+    },
+    {
+      image: slimg3,
+    },
+    {
+      image: slimg4,
+    },
+    
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div id="/" className="pt-10">
-      <div className="lg:hidden md:hidden">
-        <Carousel
-          autoPlay={true}
-          showArrows={false}
-          infiniteLoop={true}
-          interval={8000}
-          stopOnHover={false}
-          showThumbs={false}
-          showStatus={false}
-          showIndicators={false}
-          swipeable={false}
-        >
-          <MainSlider sliderImg={SlImgSm1} />
-        </Carousel>
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].image})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      {/* Left Arrow*/}
+      <div
+        className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5
+       text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+      >
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
-
-      <div className="sm:hidden">
-        <Carousel
-          autoPlay={true}
-          showArrows={false}
-          infiniteLoop={true}
-          interval={8000}
-          stopOnHover={false}
-          showThumbs={false}
-          showStatus={false}
-          showIndicators={false}
-          swipeable={false}
-        >
-          <MainSlider sliderImg={SlImg1} />
-        </Carousel>
+      {/* Right Arrow*/}
+      <div
+        className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5
+       text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+      >
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
-      <div className="mb-10">
-        <Certifications />
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="test-2xl cursor-pointer"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
       </div>
     </div>
   );
-};
-
-export default Main;
+}
+export default App;
