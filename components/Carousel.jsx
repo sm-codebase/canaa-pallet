6a72c4fb6f93658e2a1d656b04053da49a67d776
willian.m.sousa@gmail.com <willian.m.sousa@gmail.com>
2023-12-12 20:31:54 -0300

    Imagens locais

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,23 +1,17 @@
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
+import primg1 from "../public/assets/carousel/pr-img-1.png";
+import primg2 from "../public/assets/carousel/pr-img-2.png";
+
+const slides = [
+  { id: 1, src: primg1, alt: 'Image 1' },
+  { id: 2, src: primg2, alt: 'Image 2' },
+  
+];
 
 function Carousel() {
-  const slides = [
-    {
-      url: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?auto=compress&cs=tinysrgb&w=600'
-    },
-    {
-      url: 'https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=600',
-    },
-    {
-      url: 'https://images.pexels.com/photos/13009437/pexels-photo-13009437.jpeg?auto=compress&cs=tinysrgb&w=600'
-    },
-    {
-      url: 'https://images.pexels.com/photos/2156/sky-earth-space-working.jpg?auto=compress&cs=tinysrgb&w=600'
-    },
-  ];
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
 
@@ -54,14 +48,20 @@ function Carousel() {
 
   return (
     <div
-      className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group"
+      className="max-w-[1400px] h-[780px] w-full m-auto py- px-4 relative group"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className="w-full h-full mt-11 rounded-2xl bg-center bg-cover duration-500"
-      ></div>
+      <div className="w-full h-full mt-11 rounded-2xl bg-center bg-cover duration-500">
+        <Image
+          src={slides[currentIndex].src}
+          alt={`Slide ${slides[currentIndex].id}`}
+          layout="fill"
+          objectFit="cover"
+          priority // Add the priority property
+        />
+      </div>
+
       {/* Left Arrow*/}
       <div
         className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5
@@ -77,10 +77,10 @@ function Carousel() {
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
       <div className="flex top-4 justify-center py-2">
-        {slides.map((slide, slideIndex) => (
+        {slides.map((slide) => (
           <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
+            key={slide.id}
+            onClick={() => goToSlide(slide.id - 1)}
             className="test-2xl cursor-pointer"
           >
             <RxDotFilled />
