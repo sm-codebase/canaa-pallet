36ce30847d55175fc7dd2028f356335843b422b5
willian.m.sousa@gmail.com <willian.m.sousa@gmail.com>
2025-08-26 10:24:39 -0300

    chore: adiciona Google Ads tag (gtag.js) no _document.js para rastreamento de conversões

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link 
-            href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" 
-            rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" 
+          rel="stylesheet"
+        />
+
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17509633248"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17509633248');
+            `,
+          }}
         />
       </Head>
       <body>
